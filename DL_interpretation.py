0b29303515f90fe9468da3825a7fd58726ff0b7d
erginisleyen <43764947+erginisleyen@users.noreply.github.com>
2020-11-24 07:21:12 -0700

    Update and rename captum_interpret.py to DL_interpretation.py

--- DL_interpretation.py
+++ DL_interpretation.py
@@ -45,19 +45,8 @@ class ImageFolderWithPaths(datasets.ImageFolder):
         # make a new tuple that includes original and the path
         tuple_with_path = (original_tuple + (path,))
         return tuple_with_path
-'''
-# EXAMPLE USAGE:
-# instantiate the dataset and dataloader
-data_dir = "your/data_dir/here"
-dataset = ImageFolderWithPaths(data_dir) # our custom dataset
-dataloader = torch.utils.DataLoader(dataset)
 
-# iterate over data
-for inputs, labels, paths in dataloader:
-    # use the above variables freely
-    print(inputs, labels, paths)
-'''
-data_dir = 'rgb/val/'
+data_dir = 'example/'
 
 image_dataset = ImageFolderWithPaths(root=data_dir, transform=transform)
 testloader = torch.utils.data.DataLoader(image_dataset,batch_size=198,shuffle=False, num_workers=0)
@@ -96,44 +85,19 @@ for ind in range(0,198):
                                                  )
         return tensor_attributions
     
-    '''
-    saliency = Saliency(model)
-    grads = saliency.attribute(input, target=labels[ind].item())
-    grads = np.transpose(grads.squeeze().cpu().detach().numpy(), (1, 2, 0))
-    
-  
     ig = IntegratedGradients(model)
     attr_ig, delta = attribute_image_features(ig, input, baselines=input * 0, return_convergence_delta=True)
     attr_ig = np.transpose(attr_ig.squeeze().cpu().detach().numpy(), (1, 2, 0))
     print('Approximation delta: ', abs(delta))
-    '''
-    ig = IntegratedGradients(model)
-    nt = NoiseTunnel(ig)
-    attr_ig_nt = attribute_image_features(nt, input, baselines=input * 0, nt_type='smoothgrad_sq',
-                                          n_samples=10, stdevs=0.2)
-    attr_ig_nt = np.transpose(attr_ig_nt.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
-    '''
-    dl = DeepLift(model)
-    attr_dl = attribute_image_features(dl, input, baselines=input * 0)
-    attr_dl = np.transpose(attr_dl.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
-    '''
     
     print('Original Image')
     print('Predicted:', classes[predicted[ind]], 
           ' Probability:', torch.max(F.softmax(outputs, 1)).item())
     
     original_image = np.transpose((inputs[ind].cpu().detach().numpy() / 2) + 0.5, (1, 2, 0))
-    
-    #_ = viz.visualize_image_attr(None, original_image, method="original_image", title="Original Image")
-    
-    #_ = viz.visualize_image_attr(grads, original_image, method="blended_heat_map", sign="absolute_value",show_colorbar=True, title="Overlayed Gradient Magnitudes")
-   
-    # _ = viz.visualize_image_attr(attr_ig, original_image, method="blended_heat_map",sign="all",show_colorbar=True, title="Overlayed Integrated Gradients")
-    
-    _ = viz.visualize_image_attr(attr_ig_nt,original_image, method="original_image", sign="absolute_value", outlier_perc=2,fig_size=(8,8), show_colorbar=False)
-    
-    #_ = viz.visualize_image_attr(attr_dl, original_image, method="blended_heat_map",sign="all",show_colorbar=True, title="Overlayed DeepLift")
-    
-    plt.savefig('Figures_rgb/fig'+str(ind)+'.png',bbox_inches='tight',pad_inches=0)
+       
+    _ = viz.visualize_image_attr(attr_ig, original_image, method="blended_heat_map",sign="all",show_colorbar=True, title="Overlayed Integrated Gradients")
+      
+    plt.savefig('Figures/fig'+str(ind)+'.png',bbox_inches='tight',pad_inches=0)
         
     ind = ind +1
